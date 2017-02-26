4a29dec5ffec522349f1314240c2eecb18199d7e
viter <chekhiv@gmail.com>
2017-02-26 22:41:00 +0200

    CRUD functionality added

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,46 @@
 module.exports = app => {
-    const Tasks = app.models.tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}, (tasks) => {
-           res.json({tasks: tasks}); 
+    const Tasks = app.db.models.Tasks;
+    app.route("/tasks")
+        .get((req, res) => {
+            Tasks.findAll({})
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
+        })
+        .post((req, res) => {
+            Tasks.create(req.body)
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
         });
-    });
+    app.route("/tasks/:id")
+        .get((req, res) => {
+            Tasks.findOne({where: req.params})
+                .then(result => {
+                    if(result) {
+                        res.json(result);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                })
+        })
+        .put((req, res) => {
+            Tasks.update(req.body, {where: req.params})
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                })
+        })
+        .delete((req, res) => {
+            Tasks.destroy({where: req.params})
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
+        })
 };
